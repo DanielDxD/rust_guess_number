2ff13c6b358bddfd8bb49d5aae7839e3d7521b6f
Daniel Lopes <daniel.dxdb@gmail.com>
2024-03-07 15:01:55 -0300

    Reading input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number:");
+
+    println!("Write a number: ");
+    let mut guess: String = String::new();
+
+     io::stdin()
+         .read_line(&mut guess)
+         .expect("Can't read your number");
+
+    println!("Você chutou: {guess}");
 }
